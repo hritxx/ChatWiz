381de363cfa7e744c791c109ac36466f1e7961a2
hritxx <hriteekroy1869@gmail.com>
2024-05-25 02:55:10 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -23,11 +23,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(5000, () => {
   connectToMongoDB();
